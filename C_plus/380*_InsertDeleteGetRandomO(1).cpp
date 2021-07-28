01edb177b87e01098d53bded71fc1a4ca22e3430
a920604a <a920604a@gmail.com>
2021-07-28 22:44:35 +0800

    常數時間刪除/搜尋 數組中任意元素

--- C_plus/380*_InsertDeleteGetRandomO(1).cpp
+++ C_plus/380*_InsertDeleteGetRandomO(1).cpp
@@ -1,43 +1,42 @@
-class RandomizedSet
-{
+class RandomizedSet {
 private:
+    unordered_map<int,int> valToIndex;
     vector<int> nums;
-    unordered_map<int, int> m;
-
 public:
     /** Initialize your data structure here. */
-    RandomizedSet()
-    {
+    RandomizedSet() {
     }
-
+    
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    bool insert(int val)
-    {
-        if (m.count(val))
-            return false;
-        nums.emplace_back(val);
-        m[val] = nums.size() - 1;
+    bool insert(int val) {
+        // O(1)
+        if(valToIndex.count(val)) return false;
+        
+        nums.push_back(val);
+        valToIndex[val] = nums.size()-1;
         return true;
     }
-
+    
     /** Removes a value from the set. Returns true if the set contained the specified element. */
-    bool remove(int val)
-    {
-        if (!m.count(val))
-            return false;
-        // 先將要刪除數字移至尾部，再加其刪除。
-        int last = nums.back();
-        m[last] = m[val];
-        nums[m[val]] = last;
-        // 刪除
+    bool remove(int val) {
+        // O(1)，在數組中，要刪除元素與末端元素 先進行交換下進行刪除
+        if(valToIndex.find(val) ==valToIndex.end() ) return false;
+        // 取得 val 的索引
+        int i = valToIndex[val];
+        // 修改 nums.back() 的索引成 val 的索引，因為稍後會對調
+        valToIndex[nums.back()] = i;
+        
+        swap(nums[i] , nums.back());
+        
         nums.pop_back();
-        m.erase(val);
+        // 刪除 val 對應的索引
+        valToIndex.erase(val);
         return true;
     }
-
+    
     /** Get a random element from the set. */
-    int getRandom()
-    {
+    int getRandom() {
+        // O(1)
         return nums[rand() % nums.size()];
     }
 };
