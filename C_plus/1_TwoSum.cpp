6710a987e85584831ff0b75fa230040e3ed574c7
a920604a <a920604a@gmail.com>
2021-07-29 19:27:41 +0800

    2021/07/29

--- C_plus/1_TwoSum.cpp
+++ C_plus/1_TwoSum.cpp
@@ -1,15 +1,34 @@
-/*
- * @Author: yuan
- * @Date: 2021-03-20 15:06:39
- * @LastEditTime: 2021-07-15 14:14:59
- * @FilePath: /leet_code/C_plus/1_TwoSum.cpp
- */
+class TwoSum
+{
+private:
+    set<int> freq;
+    list<int> nums;
+
+public:
+    void add(int number)
+    {
+        // 紀錄所有可能組了的和
+        for (int n : nums)
+        {
+            freq.insert(n + number);
+        }
+        nums.push_back(number);
+    }
+    // 設計API，應該考慮find可能被運行多次，不可能每次都要O(N)，優化成O(1)
+    bool find(int value)
+    {
+        return freq.count(value);
+    }
+};
+
 class Solution
 {
+
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
         // https://web.stanford.edu/class/cs9/sample_probs/TwoSum.pdf
+
         // option 1 brute O(n^2) time and O(1) space
         // int n = nums.size();
         // for(int i=0;i<n;++i){
@@ -19,20 +38,20 @@ public:
         // }
         // return {};
 
-        option 2 Hashing O(n) time and O(n) space
-        map<int, int> tmp;
+        // option 2 Hashing O(n) time and O(n) space
+        unordered_map<int, int> m;
         for (int i = 0; i < nums.size(); i++)
         {
 
-            if (tmp.find(target - nums.at(i)) != tmp.end())
+            if (m.count(target - nums.at(i)))
             {
-                return {tmp[target - nums.at(i)], i};
+                return {m[target - nums.at(i)], i};
             }
-            tmp[nums.at(i)] = i;
+            m[nums.at(i)] = i;
         }
         return {};
 
-        
-
+        // option 3 API interface
+        // 170. Two Sum III - Data structure design
     }
 };
